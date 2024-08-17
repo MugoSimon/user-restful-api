d6a2432ebb091801f88da565e2fbcd72fd28afbe
Simon.Mugo <mugolastbon@gmail.com>
2024-08-17 13:08:16 +0300

    documentation in main class

--- user-restful-api/src/main/java/com/mugosimon/user_restful_api/UserRestfulApiApplication.java
+++ user-restful-api/src/main/java/com/mugosimon/user_restful_api/UserRestfulApiApplication.java
@@ -1,6 +1,11 @@
 package com.mugosimon.user_restful_api;
 
 import com.mugosimon.user_restful_api.utils.PortListener;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -9,6 +14,26 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "User - Restful - Api",
+                version = "v1.0.0",
+                description = "User - Restful - Api",
+                contact = @Contact(
+                        name = "Simon Wangechi",
+                        email = "mugolastbon@gmail.com",
+                        url = "https://github.com/mugosimon"),
+                termsOfService = "http://swagger.io/terms/",
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "http://com.mugosimon"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Spring Boot User Management API",
+                url = "https://github.com/mugosimon"
+        )
+)
 @SpringBootApplication
 public class UserRestfulApiApplication implements ApplicationListener<ApplicationReadyEvent> {
 
